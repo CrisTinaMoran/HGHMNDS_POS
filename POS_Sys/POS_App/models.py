7e186c37046f49249d6f9ff7efedc4e1a6ef11e1
Khirvie <khirviecliffordbautista@gmail.com>
2024-11-14 20:24:45 +0800

    Updated

--- POS_Sys/POS_App/models.py
+++ POS_Sys/POS_App/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import users
 
-# Create your models here.
+class Item(models.Model):
+    Prd_name = models.CharField(max_length=max)
+    Description =  models.TextField()
+    price = models.IntegerField()
+    qty =  models.Intergerfield()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name 
+    
+    
